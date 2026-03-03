4dd2c62cb5a83673c9c7960060154a0dd9f61240
lordcyd <cyril.grant@icloud.com>
2026-03-03 22:15:16 +0000

    Added Maps API keys and rebuilt project.

---
+++ android/app/src/main/kotlin/com/example/cyds_hgv_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cyds_hgv_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import Flutter
+import UIKit
+import GoogleMaps
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+      GMSServices.provideAPIKey("AIzaSyBLhwZwl86vSoeQZ3pQeUomMqgogM1WQMI")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+}
